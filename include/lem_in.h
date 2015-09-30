eaa321c4fa0d12e6fbb8fd577bc45b579ba87a00
ulefebvr <ulefebvr@e1r1p8.42.fr>
2015-09-30 17:40:51 +0200

    tools_maj

--- include/lem_in.h
+++ include/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/30 14:32:44 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/09/30 15:46:50 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/09/30 17:39:50 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,21 @@ typedef struct				s_lem
 	int						link[128];
 	int						start;
 	int						end;
+	int						stat;
+	int						coord_x;
+	int						coord_y;
 	struct s_lem			*next;
 }							t_lem;
+
+typedef struct				s_info
+{
+	t_lem					*list;
+	int						no_ant;
+}							t_info;
+
+int							ft_tablen(char **tab);
+void						ft_freetab(char **tab);
+
+t_info						*ft_parse(void);
 
 #endif
