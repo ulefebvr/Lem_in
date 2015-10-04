49cc38ff6afc7292b05e51b2c0c55c5f0d66c50f
ulefebvr <ulefebvr@e1r1p8.42.fr>
2015-10-04 21:31:47 +0200

    main test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/02 10:17:05 by ulefebvr          #+#    #+#             */
-/*   Updated: 2015/10/03 17:52:00 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2015/10/04 21:12:53 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ t_lem	*get_endroom(t_lem *list)
  int			main(void)
  {
  	t_info	*info;
+ 	t_paths	*paths;
  	t_path	*path;
 	int		i;
  
@@ -59,15 +60,27 @@ t_lem	*get_endroom(t_lem *list)
  	// 	list = list->path;
  	// }
 
- 	pathfinder(info);
- 	info->path = get_path(info);
- 	path = info->path;
- 	while (path)
+	info->no_path = 0;
+ 	info->paths = get_allpaths(info, NULL);
+ 	// sendtopath(info);
+ 	set_prorata_len(info);
+ 	paths = info->paths;
+ 	printf("how much path available === %d\n", info->no_path);
+ 	while (paths)
  	{
- 		printf("%s\n", path->room->name);
- 		path = path->next;
+ 		printf("path len : %d\n", paths->len);
+ 		printf("max to pass %d\n", paths->max);
+ 		path = paths->path;
+ 		while (path)
+ 		{
+	 		printf("%s\n", path->room->name);
+	 		path = path->next;
+	 	}
+	 	printf("===============\n");
+	 	paths = paths->next;
  	}
-
+ 	printf("\n\n\n");
+ 	sendtopath(info);
  	ft_exit(info);
  
  
