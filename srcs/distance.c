d3adb77d3fdd10ea493d33b4711d358c7f532569
ulefebvr <ulef45av@gmail.com>
2015-10-05 14:46:55 +0200

    norme

--- srcs/distance.c
+++ srcs/distance.c
@@ -5,6 +5,7 @@ double		distance(t_lem *a, t_lem *b)
 {
 	double dist;
 
-	dist = (double)sqrt( pow((b->coord_x - a->coord_x), 2) + pow((b->coord_y - a->coord_y), 2));
+	dist = (double)sqrt( pow((b->coord_x - a->coord_x), 2)
+		+ pow((b->coord_y - a->coord_y), 2));
 	return (dist);
 }
