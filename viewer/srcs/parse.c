d89db54ef6c2fecc026ff96de7124225bf3fcf71
zipotorof <rclanget@student.42.fr>
2015-10-05 15:18:16 +0200

    parse update

--- viewer/srcs/parse.c
+++ viewer/srcs/parse.c
@@ -40,25 +40,10 @@ static int	check_node()
 
 int	parse(void)
 {
-	t_map *begin;
-	t_links *link_begin;
-
 	if (!check_node())
 	{
 		write(2, "ERROR\n", 6);
 		exit(0);
-	}
-	begin = ft_global(NULL);
-	while (begin)
-	{
-		printf("name: %s\nnum: %d\nx: %d\ny: %d\nstart: %d\nend: %d\n", begin->name, begin->num, begin->x, begin->y, begin->start, begin->end);
-		link_begin = begin->links;
-		while (link_begin)
-		{
-			printf("         node => %d\n\n", link_begin->node);
-			link_begin = link_begin->next;
-		}
-		begin = begin->next;
 	}
 	return (1);
 }
